6afe62afc6db6ed8fcf66ba2f8d7b125d15101bb
Samuel Hurtado <lnx.wifi@gmail.com>
2024-08-09 08:55:49 +0200

    util functions

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,8 +6,24 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 08:23:58 by shurtado          #+#    #+#             */
-/*   Updated: 2024/08/09 08:24:20 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/08/09 08:55:34 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+int ft_putstr(const char *str)
+{
+	int	i;
+
+	if (!str)
+		return(ft_putstr("(null)"));
+	i = 0;
+	while (str[i])
+		i++;
+	return (write(1, str, i));
+}
