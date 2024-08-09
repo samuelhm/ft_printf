81a1749dc8dafd4001e2e12c8bd3cb1a54270f67
Samuel Hurtado <lnx.wifi@gmail.com>
2024-08-09 08:21:55 +0200

    header prototypes

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 10:13:29 by shurtado          #+#    #+#             */
-/*   Updated: 2024/08/09 07:28:52 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/08/09 08:21:30 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_PRINTF_H
@@ -17,6 +17,12 @@
 #include <stdarg.h>
 
 int	ft_printf(char const *, ...);
+int	ft_putchar(char c);
+int	ft_putstr(const char *str);
+int	ft_putnbr(int nb);
+int	ft_putunbr(unsigned int nb);
+int ft_puthxnbr(unsigned int nb,int upper);
+int ft_putptr(unsigned long nb);
 
 #endif
 
