849a158bfa16a96f12c5f227236701e9a3a7aa62
Samuel Hurtado <lnx.wifi@gmail.com>
2024-08-09 09:16:57 +0200

    added putnbr and putunbr

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 08:23:58 by shurtado          #+#    #+#             */
-/*   Updated: 2024/08/09 08:55:34 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/08/09 09:16:43 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int ft_putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
 int ft_putstr(const char *str)
 {
 	int	i;
@@ -26,4 +27,30 @@ int ft_putstr(const char *str)
 	while (str[i])
 		i++;
 	return (write(1, str, i));
+}
+
+int ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		return (write(1, "-2147483648",11));
+	if (nb < 0)
+	{
+		return (ft_putnbr(nb * -1) + write(1, "-", 1));
+	}
+	if (nb >= 10)
+	{
+		return (ft_putnbr(nb / 10) + ft_putnbr(nb % 10));
+	}
+	else
+	{
+		return (ft_putchar(nb + 48));
+	}
+}
+
+int ft_putunbr(unsigned int nb)
+{
+	if (nb >= 10)
+		return (ft_putunbrt(nb / 10));
+	else
+		return (ft_putchar((nb % 10) + 48));
 }
