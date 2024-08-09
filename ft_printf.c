74625a00b154d191fd2868042a286de2ddf9229a
Samuel Hurtado <lnx.wifi@gmail.com>
2024-08-09 08:22:22 +0200

    main function

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,41 @@
 /*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 10:13:51 by shurtado          #+#    #+#             */
-/*   Updated: 2024/08/09 07:28:34 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/08/09 08:17:24 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(char const *, ...)
-{
+int	printarg(char carg, va_list args);
 
+int	ft_printf(char const *str, ...)
+{
+	int		i;
+	va_list	args;
+	int		result;
+
+	va_start(args, str);
+	i = 0;
+	result = 0;
+
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			result += printarg(str[i], args);
+		}
+		else
+		{
+			if(!write(1, &str[i], 1))
+			{
+				va_end(args);
+				return (-1);
+			}
+			result++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (result);
 }
